1bd62e1d44ea83d5d1bbb8ce44338e0ca56962f8
your_username <h.murakamidepaula@usp.br>
2022-05-23 23:57:25 -0300

    Initial commit

--- PiupiuerJS.js
+++ PiupiuerJS.js
@@ -1,16 +1,22 @@
 function deletarPiu(id){
-    console.log(id)
     document.getElementById(`${id}`).classList.add("hidden")
 }
 
-function Favoritar(){
-    document.querySelector(".Estrela").src = "estrelaAmarela.jpg"
-    document.querySelector(".BotaoFavoritar").style.opacity = "0.8"
-}
 
-function DarLike(){
-    document.querySelector(".Coracao").src = "CoracaoVermelho.png"
-    document.querySelector(".BotaoLike").style.opacity = "0.8"
+function DarLike(id){
+    const idElemento = document.getElementById(`${id}`)
+    const contador  = idElemento.querySelector(".contador").innerHTML
+
+    if(idElemento.querySelector(".Coracao").src === "file:///C:/Users/User/Documents/html-css-js-HeMuPa/Imagens/CoracaoVermelho.png"){
+        idElemento.querySelector(".Coracao").src = "Imagens/heart.svg"
+        idElemento.querySelector(".BotaoLike").style.opacity = "0.5"
+        idElemento.querySelector(".contador").innerHTML = parseInt(contador) - 1
+    }
+    else{
+        idElemento.querySelector(".Coracao").src = "Imagens/CoracaoVermelho.png"
+        idElemento.querySelector(".BotaoLike").style.opacity = "0.8"
+        idElemento.querySelector(".contador").innerHTML = parseInt(contador) + 1
+    }
 }
 
 const PiusFeed = document.querySelector(".PiusFeed");
@@ -29,16 +35,17 @@ async function getPost(){
         `<div class="CaixaPiuFeed" id = "${post.id}"> 
 
             <div class="ImagemEUser">
-                <img src="${post.user.photo}" onerror = "src = 'ImagemPerfil.svg'" height="75px" width="78px" class="ImagemUser">
+                <img src="${post.user.photo}" onerror = "src = 'Imagens/ImagemPerfil.svg'" height="75px" width="78px" class="ImagemUser">
                 <p class="User"> <strong>${post.user.first_name} ${post.user.last_name}</strong> </p> <span class="Arroba">@${post.user.username} </span> </p>
             </div>
             <p class="TextoPiu">${post.text}</p>
             <div class="IconesInteracao">
                 <div class="BotaoLike">
-                    <button onclick="DarLike()"><img src="heart.svg" height="32px" width="32px" class="Coracao"></button>
+                    <p class = "contador">0</p>
+                    <button onclick="DarLike('${post.id}')" class="botaoLike"><img src="Imagens/heart.svg" height="32px" width="32px" class="Coracao"></button>
                 </div>
-                <div class="BotaoFavoritar">
-                    <button onclick="deletarPiu('${post.id}')"> <img src="Lixeira.png" height="32px" width="32px" class="Estrela"> </button>
+                <div class="BotaoDeletar">
+                    <button onclick="deletarPiu('${post.id}')"> <img src="Imagens/Lixeira.png" height="32px" width="32px" class="Lixeira"> </button>
                 </div>
             </div>
         </div>`
@@ -48,20 +55,22 @@ async function getPost(){
 document.addEventListener("DOMContentLoaded", getPost)
 
 function PostaNoFeed(){
+    var uniq = 'id' + (new Date()).getTime();
     const text = document.querySelector("#PostPiu").value
     const html =
-        `<div class="CaixaPiuFeed">
+        `<div class="CaixaPiuFeed" id = "${uniq}">
             <div class="ImagemEUser">
-                <img src="ImagemPerfil.svg" height="75px" width="78px" class="ImagemUser">
+                <img src="Imagens/ImagemPerfil.svg" height="75px" width="78px" class="ImagemUser">
                 <p class="User"> <strong>Pejoteiro</strong> </p> <span class="Arroba">@pejoteiro123</span> </p>
             </div>
             <p class="TextoPiu">${text}</p>
             <div class="IconesInteracao">
                 <div class="BotaoLike">
-                    <button onclick="DarLike()"><img src="heart.svg" height="32px" width="32px" class="Coracao"></button>
+                <p class = "contador">0</p>
+                    <button onclick="DarLike('${uniq}')"><img src="Imagens/heart.svg" height="32px" width="32px" class="Coracao"></button>
                 </div>
-                <div class="BotaoFavoritar">
-                    <button onclick="Favoritar()"><img src="star.svg" height="32px" width="32px" class="Estrela"></button>
+                <div class="BotaoDeletar">
+                    <button onclick="deletarPiu('${uniq}')"> <img src="Imagens/Lixeira.png" height="32px" width="32px" class="Lixeira"> </button>
                 </div>
             </div>
         </div>`
@@ -103,3 +112,32 @@ const characterCounter = () => {
 
 }
 
+function TextoBusca(){
+    const html =  
+    `
+    <input placeholder="Digite sua pesquisa" onkeyup ="Pesquisa()" class = "TextoPesquisa">
+    </input>
+    `
+    document.querySelector(".BarraPesquisa").innerHTML = html
+
+}
+
+function Pesquisa(){
+    const Pius = document.querySelector(".PiusFeed").getElementsByClassName("CaixaPiuFeed")
+    const textoPesquisa = document.getElementsByClassName("TextoPesquisa")[0].value
+    console.log(textoPesquisa)
+    for(let i=0; i<Pius.length; i++){
+        const username = Pius[i].getElementsByClassName("Arroba")[0].innerHTML
+        const name = Pius[i].getElementsByClassName("User")[0].getElementsByTagName("strong")[0].innerHTML
+        if(filtraPesquisa(textoPesquisa,username) || filtraPesquisa(textoPesquisa,name)){
+            Pius[i].style.display = "flex"
+        }
+        else{
+            Pius[i].style.display = "none"
+        }
+    }
+}
+
+function filtraPesquisa(textoDigitado, textoFeed){
+    return textoFeed.toLowerCase().includes(textoDigitado.toLowerCase())
+}
